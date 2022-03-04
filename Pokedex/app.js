c3d848482e8b820c7090730cbc32f34f762e1971
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-04 15:36:56 -0300

    1°Commit

---
+++ Pokedex/app.js
@@ -0,0 +1,36 @@
+const fetchPokemon = ()=>{
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromisses = []
+
+    for(let i = 1; i<=150; i++){
+       pokemonPromisses.push(fetch(getPokemonUrl(i)).then(response=>response.json()))
+    }
+
+    Promise.all(pokemonPromisses)
+    .then(pokemons =>{
+        const lisPokemons = pokemons.reduce((accumulator,pokemon)=>{
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            
+            accumulator +=`
+            <li class="card ${types[0]}">
+            <img glass="card-image " alt="${pokemon.name}"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+            <h2 class="card-title">
+            ${pokemon.id}.${pokemon.name} 
+            </h2>
+            <p class ="card-subtitle">${types.join(' | ')} </p>
+            </li>
+            `
+            return accumulator
+        },'')
+
+        const ul = document.querySelector('[data-js="pokedex"]')
+
+        ul.innerHTML = lisPokemons
+    })
+
+    
+}
+
+fetchPokemon()
